b03f3cb5e21bb28ac1996a2f5aae3154c71b3eb0
suryar <suryar@qburst.com>
2016-02-04 16:10:49 +0530

    to gh-pages

---
+++ js/sript1.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	/* var companyData_url = "../json/techParkCompanies.json";
+     var companies = "";  
+	 $.getJSON(companyData_url, function(companyData) {
+	  companies = JSON.stringify(companyData);
+	  console.log("companies: "+companies);
+	});
+
+     var companyListTbodyElement = $(document).getElementById("companyListBody");
+     var i = 0;
+     var companyCount = 10;
+     for(i=0;i<10;i++){
+     	var tbodyData = "<tr><td>"+(i+1)+"</td><td>"+companies[i].name+"</td><td>"+
+     	                 companies[i].headOffice+
+     	                 "</td><td><img src='images/view-details.jpg' width='20px' height='20px' id='viewDetails'>
+     	                  </td></tr>";
+     	companyListTbodyElement.innerHTML = tbodyData;
+     } 
+*/
+
+
+
+
+
+ 
+
+});
